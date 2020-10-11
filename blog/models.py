2acec94875e3e72ea34fcbbae903dad993105d82
Leshkin <gerda.le.shanti@gmail.com>
2020-10-12 02:04:18 +0700

    Created & added to admin Post model for Blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Publish"))
+
+
+class Post(models.Model):
+    post_title = models.CharField(max_length=200, unique=True)
+    post_text = models.TextField()
+    post_date = models.DateTimeField(auto_now_add=True)
+    post_image = models.ImageField(upload_to="post_images/")
+    post_status = models.IntegerField(choices=STATUS, default=0)
